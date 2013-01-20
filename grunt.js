f70d59664c1dba517806ef18c4e7663bb37885b1
hongymagic <davidhong.code@gmail.com>
2013-01-20 21:55:29 +1100

    add grunt task

---
+++ grunt.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+// Project configuration
+
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+
+// Configure LESS => CSS
+
+		less: {
+			development: {
+				options: {
+					paths: ['public/css/less']
+				},
+				files: {
+					'public/css/styles.css': 'public/css/less/*.less'
+				}
+			},
+
+			productions: {
+				options: {
+					paths: ['public/css/less']
+				},
+				files: {
+					'public/css/styles.css': 'public/css/less/*.less'
+				}
+			}
+		}
+	});
+
+// Load npm tasks
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	grunt.registerTask('default', 'less');
+
+};
